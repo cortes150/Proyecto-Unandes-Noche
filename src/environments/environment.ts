6a662b069e1c2430cf9eeb4009db8c1f63a72dc3
cortes150 <chicholas150@gmail.com>
2024-09-24 20:56:30 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,3 @@
 export const environment = {
-  firebaseConfig: {
-    apiKey: 'AIzaSyAhNXc2CbWE9EipBomKz3TIPADKBTftEEA',
-    authDomain: 'proyecto-unandes-noche.firebaseapp.com',
-    projectId: 'proyecto-unandes-noche',
-    storageBucket: 'proyecto-unandes-noche.appspot.com',
-    messagingSenderId: '611968465054',
-    appId: '1:611968465054:web:ce8bc0c3d8e20f6040e38f',
-  },
+  
 };
